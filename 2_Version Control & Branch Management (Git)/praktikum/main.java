48c6549b0bb1f6e6322e36901e73aa4d58877308
handiraleo <dsinaga9@gmail.com>
2022-02-22 11:47:24 +0700

    inisialisasi project

---
+++ 2_Version Control & Branch Management (Git)/praktikum/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+  
+      System.out.println("Hai");
+      System.out.println("aku leo");
+      System.out.println("dor");
+    
+    }
+  
+  }
